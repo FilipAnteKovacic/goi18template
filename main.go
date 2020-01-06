e751ee83f474a82f8cda6c1e108edda0a0bfdb6e
Filip-Ante Kovacic <filip.ante.kovacic@gmail.com>
2020-01-06 10:46:38 +0100

    Add comments

--- main.go
+++ main.go
@@ -9,20 +9,25 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// Contain all translations
 var i18Map map[string]string
 
 var router *httprouter.Router
 
+// Use to define template functions
 var templateFuncs map[string]interface{}
 
 func init() {
 
 	var err error
+
+	// Parse translations
 	i18Map, err = parseJSONi18File("en.json")
 	if err != nil {
 		fmt.Println("error while parsing file", err)
 	}
 
+	// Define template funcs
 	templateFuncs = template.FuncMap{
 		"i18": i18,
 	}
@@ -44,6 +49,7 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
+	// Init base temaplate
 	baseTemplate, err := template.
 		New("index.html").
 		Funcs(templateFuncs).
@@ -55,6 +61,7 @@ func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		return
 	}
 
+	// Cloning from base template
 	contentTemplate, err := template.
 		Must(baseTemplate.Clone()).
 		ParseFiles(
@@ -75,6 +82,7 @@ func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 func login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
+	// Init another template
 	loginTemplate, err := template.
 		New("login.html").
 		Funcs(templateFuncs).
